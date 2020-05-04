03db5640bf706db655c9df151bb41dd95eda2ef6
Oner Elmas <oelmas@argosai.com>
2020-05-04 17:02:36 +0300

    shop page added, collection preview and collection item components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "../src/pages/homepage/homepage.component";
+import ShopPage from "../src/pages/shop/shop.component.jsx";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import CollectionPreview from "../../components/collections-preview/collections-preview.component.jsx";
+
+import SHOP_DATA from "./shop.data.js";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollections }) => (
+          <CollectionPreview key={id} {...otherCollections} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
