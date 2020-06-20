8e0db41a7cb77972e9c3a00e7ca20b54e24e2294
Oner Elmas <oelmas@argosai.com>
2020-06-20 21:18:52 +0300

    Added  addItem and removeItem functionality to checkout component

--- src/reducer/cart/cart.actions.js
+++ src/reducer/cart/cart.actions.js
@@ -1,5 +1,18 @@
-import CartActionTypes from './cart.types';
+import CartActionTypes from "./cart.types";
 
-export const toggleCart = () => ( { type: CartActionTypes.TOGGLE_CART } );
+export const toggleCart = () => ({ type: CartActionTypes.TOGGLE_CART });
 
-export const addItem = ( item ) => ( { type: CartActionTypes.ADD_ITEM, payload: item } )
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
